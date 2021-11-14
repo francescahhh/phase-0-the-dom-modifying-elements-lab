dd814f942c7bfe5e753e23f28b8e75bb6e797830
Francesca Hamm <francesca.c.hamm@gmail.com>
2021-11-13 23:43:54 -0700

    commit tree d3d908c51cd90f8fb2fb71c1ffa97d81c1a14f79

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+//       it("has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside", () => {
+//         expect(newHeader.innerHTML, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
+//       });
+  
+//     });
+//   })
+
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = 'francesca is the champion';
+console.log(newHeader.textContent);
